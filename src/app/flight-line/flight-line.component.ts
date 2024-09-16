2efd56b6ce8519c711c5ecc5ae87023ead4df776
AlisaZil <alisazilberman@gmail.com>
2024-09-16 09:44:33 -0700

    add(interface for flight )

--- src/app/flight-line/flight-line.component.ts
+++ src/app/flight-line/flight-line.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Flight } from '../interfaces';
 
 @Component({
   selector: 'app-flight-line',
   templateUrl: './flight-line.component.html',
   styleUrls: ['./flight-line.component.scss']
 })
+  
 export class FlightLineComponent {
+
+  @Input() flightData: Flight | undefined;
+
+  constructor() { }
+  
+  ngOnInit(): void {}
 
 }

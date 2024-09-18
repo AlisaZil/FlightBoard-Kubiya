096f130976083b960e00f8b255b471e57055d548
AlisaZil <alisazilberman@gmail.com>
2024-09-18 08:44:00 -0700

    add(flight form control, html basic stracture, placeholder func)

--- src/app/flight-board/flight-board.component.ts
+++ src/app/flight-board/flight-board.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'flight-board',
   templateUrl: './flight-board.component.html',
   styleUrls: ['./flight-board.component.scss']
 })
+  
 export class FlightBoardComponent {
+
+  public flightFormControl = this.formBuilder.group({
+    FlightNumber: new FormControl("",Validators.required),
+    Status: new FormControl(),
+    TakeoffTime: new FormControl(),
+    LandingTime: new FormControl(),
+    TakeofAirport: new FormControl(),
+    LandingAirport: new FormControl(),
+  });
+
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private formBuilder: FormBuilder) { }
+  
+  ngOnInit(): void { }
+
+  transformInputPlaceholder(formControlName: string) {
+
+    let newPlaceholder = formControlName.match(/[A-Z][a-z]+/g);
+    return newPlaceholder ? newPlaceholder.join(" ") : 'Status';
+    
+  }
+  
 
 }
